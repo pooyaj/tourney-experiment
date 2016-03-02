6fa8872e625e2972d642d6847ea3ce90852e3373
Pooya Jaferian <pooya.j@gmail.com>
2016-03-01 22:27:17 -0800

    wired up the tourney structure form to keep temporary state.

--- src/components/structure/Structure.jsx
+++ src/components/structure/Structure.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 export default (props) => {
-  const {level, bb, sb, ante} = props;  
+  const {level, bb, sb, ante, onFieldUpdate} = props;  
   return <div className='row'>
     <div className="col-xs-2 col-md-2"> {level} </div>
-    <div className="col-xs-3 col-md-3"> <input type="text" value={bb} /> </div>
-    <div className="col-xs-3 col-md-3"> <input type="text" value={sb} /> </div>
-    <div className="col-xs-3 col-md-3"> <input type="text" value={ante} /></div>
+    <div className="col-xs-3 col-md-3"> 
+      <input type="text" 
+        defaultValue={bb} 
+        onChange={onFieldUpdate('bb')}/> 
+    </div>
+    <div className="col-xs-3 col-md-3"> 
+      <input type="text" 
+        defaultValue={sb}
+        onChange={onFieldUpdate('sb')}/>       
+    </div>
+    <div className="col-xs-3 col-md-3"> 
+      <input type="text" 
+        defaultValue={ante}
+        onChange={onFieldUpdate('ante')}/>
+    </div>
   </div>;
 }
--- src/components/structure/StructureList.jsx
+++ src/components/structure/StructureList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Structure from './Structure';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux'
-import {toJS} from 'immutable'
+import {curry} from 'lodash'
 
 const structureList = React.createClass({
   mixins: [PureRenderMixin],
@@ -10,29 +10,33 @@ const structureList = React.createClass({
     return this.props.structure || [];
   },
   render: function() {
-    var structure = this.getData();
-    console.log(structure.get(1));
+    const structure = this.getData();
+    const updater = curry(this.onFieldUpdate);
     return <div>            
       {structure.map(
         (item, key) => <Structure key={key} 
                                   level={key} 
                                   bb={item.get('bb')}
                                   sb={item.get('sb')}
-                                  ante={item.get('ante')} 
+                                  ante={item.get('ante')}
+                                  onFieldUpdate={updater(key)} 
                         />)}
     </div>;
-  }
+  }, 
+  onFieldUpdate: function (index, field, value) 
+  {        
+    this.tempStructure = this.tempStructure 
+      ? this.tempStructure.setIn([index, field], value.target.value) 
+      : this.props.structure;    
+  }, 
+  tempStructure: null
 });
 
-
-
-// Which action creators does it want to receive by props?
 function mapDispatchToProps(dispatch) {
   return {    
   }
 }
 
-// Which part of the Redux global state does our component want to receive as props?
 function mapStateToProps(state) {
   return {
     structure: state.structure    
